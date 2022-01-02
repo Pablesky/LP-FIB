4a181a6a257c508f8ddd7dea7df2a57d0cdce6be
Pablo Vega <pablo.vega.gallego@est.fib.upc.edu>
2022-01-02 18:08:49 +0100

    Ya hemos acabado

--- compiladores/Ramon Llull/beatVisitor.py
+++ compiladores/Ramon Llull/beatVisitor.py
@@ -12,6 +12,9 @@ init()
 
 class beatVisitor(llullVisitor):
 
+    def __init__(self):
+        self.tabs = 0
+
     def visitExpr(self, ctx):
         llistaFills = list(ctx.getChildren())
         if (len(llistaFills) == 1):
@@ -27,25 +30,189 @@ class beatVisitor(llullVisitor):
         else:
             if (llistaFills[0].getText() == "("):
                 print(Fore.WHITE + '(', end='')
-                return self.visit(llistaFills[1])
+                self.visit(llistaFills[1])
                 print(Fore.WHITE + ')', end='')
 
             else:
                 self.visit(llistaFills[0])
-                print(Fore.WHITE + llistaFills[1].getText(), end='')
+                print(' ', end='')
+                print(Fore.WHITE + llistaFills[1].getText(), end=' ')
                 self.visit(llistaFills[2])
 
     def visitAssigancio(self, ctx):
         llistaFills = list(ctx.getChildren())
-        print(Fore.WHITE + llistaFills[0].getText(), end='')
-        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        print(Fore.WHITE
+              + llistaFills[0].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[1].getText(), end=' ')
         self.visit(llistaFills[2])
-        print("\r")
 
     def visitLectura(self, ctx):
         llistaFills = list(ctx.getChildren())
-        print(Fore.BLUE + llistaFills[0].getText(), end='')
+        print(Fore.GREEN
+              + llistaFills[0].getText(), end='')
+        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        print(Fore.WHITE + llistaFills[2].getText(), end='')
+        print(Fore.WHITE + llistaFills[3].getText(), end='')
+
+    def visitBlocStrings(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        for fill in llistaFills:
+            paraula = fill.getText()
+            if paraula == ',':
+                print(Fore.WHITE + ',', end=' ')
+            elif (paraula.startswith("\"") and paraula.endswith("\"")):
+                print(Fore.BLUE + paraula, end='')
+            else:
+                self.visit(fill)
+
+    def visitEscriptura(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        print(Fore.GREEN
+              + llistaFills[0].getText(), end='')
         print(Fore.WHITE + llistaFills[1].getText(), end='')
         self.visit(llistaFills[2])
         print(Fore.WHITE + llistaFills[3].getText(), end='')
-        print("\r")
+
+    def visitBloc(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        for elem in llistaFills:
+            print('    ' * self.tabs, end='')
+            self.visit(elem)
+            print('\r')
+
+    def visitCondicion(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        self.visit(llistaFills[0])
+        print(Fore.WHITE + ' ' + llistaFills[1].getText(), end=' ')
+        self.visit(llistaFills[2])
+
+    def visitBucleWhile(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        print(Fore.RED
+              + llistaFills[0].getText(), end=' ')
+        self.tabs += 1
+        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        self.visit(llistaFills[2])
+        print(Fore.WHITE + llistaFills[3].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[4].getText(), end='')
+        print('\r')
+        self.visit(llistaFills[5])
+        self.tabs -= 1
+        print('   ' * self.tabs + Fore.WHITE
+              + llistaFills[6].getText(), end='')
+
+    def visitBucleFor(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        print(Fore.RED
+              + llistaFills[0].getText(), end=' ')
+        self.tabs += 1
+        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        self.visit(llistaFills[2])
+        print(Fore.WHITE + llistaFills[3].getText(), end=' ')
+        self.visit(llistaFills[4])
+        print(Fore.WHITE + llistaFills[5].getText(), end=' ')
+        self.visit(llistaFills[6])
+        print(Fore.WHITE + llistaFills[7].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[8].getText(), end='')
+        print('\r')
+        self.visit(llistaFills[9])
+        self.tabs -= 1
+        print('   ' * self.tabs + Fore.WHITE
+              + llistaFills[10].getText(), end='')
+
+    def visitAsigParametros(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        for fill in llistaFills:
+            paraula = fill.getText()
+            if paraula == ',':
+                print(Fore.WHITE + ',', end=' ')
+            else:
+                print(Fore.WHITE + paraula, end='')
+
+    def visitAsignacionFuncion(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        self.tabs += 1
+        print(Fore.YELLOW + llistaFills[0].getText(), end=' ')
+        print(Fore.LIGHTMAGENTA_EX + llistaFills[1].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[2].getText(), end='')
+        self.visit(llistaFills[3])
+        print(Fore.WHITE + llistaFills[4].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[5].getText(), end='')
+        print('\r')
+        self.visit(llistaFills[6])
+        self.tabs -= 1
+        print('   ' * self.tabs + Fore.WHITE
+              + llistaFills[7].getText(), end='')
+        print('\r')
+
+    def visitIfCondicion(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        self.tabs += 1
+        print(Fore.RED + llistaFills[0].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        self.visit(llistaFills[2])
+        print(Fore.WHITE + llistaFills[3].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[4].getText(), end='')
+        print('\r')
+        self.visit(llistaFills[5])
+        self.tabs -= 1
+        print('   ' * self.tabs + Fore.WHITE
+              + llistaFills[6].getText(), end=' ')
+        if len(llistaFills) == 8:
+            self.visit(llistaFills[7])
+
+    def visitElseCondicion(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        self.tabs += 1
+        print(Fore.RED + llistaFills[0].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        print('\r')
+        self.visit(llistaFills[2])
+        self.tabs -= 1
+        print('   ' * self.tabs + Fore.WHITE
+              + llistaFills[3].getText(), end='')
+
+    def visitEjecParametros(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        for fill in llistaFills:
+            paraula = fill.getText()
+            if paraula == ',':
+                print(Fore.WHITE + ',', end=' ')
+            else:
+                print(Fore.WHITE + paraula, end='')
+
+    def visitEjecutarFuncion(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        print(Fore.LIGHTMAGENTA_EX + llistaFills[0].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        self.visit(llistaFills[2])
+        print(Fore.WHITE + llistaFills[3].getText(), end='')
+
+    def visitArrayCrear(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        print(Fore.GREEN + llistaFills[0].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        print(Fore.BLUE + llistaFills[2].getText(), end='')
+        print(Fore.WHITE + llistaFills[3].getText(), end=' ')
+        self.visit(llistaFills[4])
+        print(Fore.WHITE + llistaFills[5].getText(), end='')
+
+    def visitArrayGet(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        print(Fore.GREEN + llistaFills[0].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        print(Fore.BLUE + llistaFills[2].getText(), end='')
+        print(Fore.WHITE + llistaFills[3].getText(), end=' ')
+        self.visit(llistaFills[4])
+        print(Fore.WHITE + llistaFills[5].getText(), end='')
+
+    def visitArraySet(self, ctx):
+        llistaFills = list(ctx.getChildren())
+        print(Fore.GREEN + llistaFills[0].getText(), end=' ')
+        print(Fore.WHITE + llistaFills[1].getText(), end='')
+        print(Fore.BLUE + llistaFills[2].getText(), end='')
+        print(Fore.WHITE + llistaFills[3].getText(), end=' ')
+        self.visit(llistaFills[4])
+        print(Fore.WHITE + llistaFills[5].getText(), end=' ')
+        self.visit(llistaFills[6])
+        print(Fore.WHITE + llistaFills[7].getText(), end=' ')
